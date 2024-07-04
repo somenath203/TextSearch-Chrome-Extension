fdfc16fda2f77774df643dabff6826c2b8c274d5
Somenath Choudhury <somenathchoudhury35@gmail.com>
2024-07-04 16:45:32 +0530

    commited

--- background.js
+++ background.js
@@ -1,52 +1,27 @@
-const urlMap = {
-    bing: "https://www.bing.com/search?q=",
-    duckduckgo: "https://www.duckduckgo.com?q=",
-    google: "https://google.com/search?q=",
-    yahoo: "https://yahoo.com/search?q=",
-}
-
-
 const searchForTextInWeb = (selectedText) => {
 
-    chrome.storage.sync.get(['searchEngine'], (result) => {
+  chrome.tabs.create({
+    url: `https://google.com/search?q=${encodeURIComponent(selectedText)}`,
+  });
 
-        let url;
-
-        if(result.searchEngine) {
-
-            url = `${urlMap[result.searchEngine]}${encodeURIComponent(selectedText)}`;
-
-        } else {
-
-            url = `https://google.com/search?q=${encodeURIComponent(selectedText)}`
-        }
-
-        chrome.tabs.create({
-            url: url
-        });
-
-    });
-    
-}
-
+};
 
 chrome.runtime.onInstalled.addListener(() => {
 
-    chrome.contextMenus.create({
-        id: 'search-text-selection',
-        title: `Search for "%s"`,
-        contexts: ["selection"]
-    });
+  chrome.contextMenus.create({
+    id: 'search-text-selection',
+    title: 'Search for "%s"',
+    contexts: ['selection'],
+  });
 
-    chrome.contextMenus.onClicked.addListener((info) => {
+});
 
-        const menuItemId = info.menuItemId;
+chrome.contextMenus.onClicked.addListener((info) => {
 
-        if(menuItemId === 'search-text-selection' && info.selectionText) {
+  if (info.menuItemId === 'search-text-selection' && info.selectionText) {
 
-            searchForTextInWeb(info.selectionText);
+    searchForTextInWeb(info.selectionText);
 
-        }
-    })
-
+  }
+  
 });
